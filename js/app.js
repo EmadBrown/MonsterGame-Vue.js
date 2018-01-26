7bf5e44650520caa5086bc52f800ec1e091d07ac
Emad-Albouni <emad.it@protonmail.com>
2018-01-26 20:07:55 +0100

    Add new feature / attacks / specialAttacks / giveUp / Heal

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ new Vue({
     data: {
         playerHealh: 100,
         monsterhealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns:[]
     },
     methods:{
         startGame:function(){
@@ -11,38 +12,77 @@ new Vue({
             this.playerHealh = 100;
             this.monsterhealth = 100;
         },
-        attack: function(){
-            var max = 10;
-            var min = 3;
-            var damage = Math.max(Math.floor(Math.random() * max)+1, min);
-            this.monsterhealth -=damage;
-
-            if(this.monsterhealth <= 0){
-                alert('You won!');
-                this.gameIsRunning = false;
+        attack: function(){ 
+            var damage = this.calculateDamage (3, 10); 
+            this.monsterhealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for ' + damage
+            });
+            if(this.checkWin()){
                 return;
             }
-
-            var max = 10;
-            var min = 3;
-            var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
-            this.playerHealh -=damage;
-
-            if(this.playerHealh <= 0){
-                alert('You lost!');
-                this.gameIsRunning = false;
-                return;
-            }
+            this.monsterAttacks();
         },
         specialAttack: function(){
-
+            var damage = this.calculateDamage (10, 20);
+            this.monsterhealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for ' + damage
+            });
+            if(this.checkWin()){
+                return;
+            }
+            this.monsterAttacks();
         },
         heal: function(){
-
+            if(this.playerHealh <= 90){
+                this.playerHealh += 10;
+            }
+            else{
+                this.playerHealh = 100;
+            }
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for 10'
+            });
+            this.monsterAttacks();
         },
         giveUp: function(){
-
+            this.gameIsRunning = false;
         },
+        calculateDamage: function(min, max){
+            return Math.max(Math.floor(Math.random() * max)+1, min);
+        },
+        monsterAttacks: function(){
+            var damage = this.calculateDamage(5, 12);
+            this.playerHealh -= damage;
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits Player for ' + damage
+            });
+            this.checkWin();
+        },
+        checkWin: function(){
+            if(this.monsterhealth <= 0){
+                if(confirm('You won! New Game')){
+                    this.startGame();
+                }else{
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            else if (this.playerHealh <= 0){
+                if(confirm('You lose! New Game')){
+                    this.startGame();
+                }else{
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false
+        }
     }
 
 });
